7f0c9817dfa15ba3357478034b1dffd18de7553f
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-01-22 12:10:30 +0200

    Task 3 adjusted

--- js/task-3.js
+++ js/task-3.js
@@ -7,21 +7,15 @@ class StringBuilder {
     return this.#value;
   }
   padStart(pad) {
-    this.#value = pad + this.#value;
+    // this.#value = pad + this.#value;
+    this.#value = `${pad}${this.#value}`;
   }
   padEnd(pad) {
-    this.#value = this.#value + pad;
+    // this.#value = this.#value + pad;
+    this.#value = `${this.#value}${pad}`;
   }
   padBoth(pad) {
-    this.#value = pad + this.#value + pad;
+    // this.#value = pad + this.#value + pad;
+    this.#value = `${pad}${this.#value}${pad}`;
   }
 }
-
-const builder = new StringBuilder('.');
-console.log(builder.getValue()); // "."
-builder.padStart('^');
-console.log(builder.getValue()); // "^."
-builder.padEnd('^');
-console.log(builder.getValue()); // "^.^"
-builder.padBoth('=');
-console.log(builder.getValue()); // "=^.^="
